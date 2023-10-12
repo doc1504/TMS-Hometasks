0e7e202be3363f25db10881e9ad7e73d6b6c7117
Anzhalika.Belotskaya <doc1504@mail.ru>
2023-10-12 20:19:10 +0300

    lesson28

---
+++ lesson28/src/main/java/org/example/service/InputService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+import org.example.domain.Bet;
+
+public interface InputService {
+    Bet inputPair();
+}
---
+++ lesson28/src/main/java/org/example/service/WalletService.java
@@ -0,0 +1,7 @@
+package org.example.service;
+
+public interface WalletService {
+
+    void add (int cost);
+    void decr(int cost);
+}
